09bba9abaf4b4b4d8ff2783c9a13eb112cfd67a5
June-Senpai <109533532+June-Senpai@users.noreply.github.com>
2023-02-19 18:04:13 +0530

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -8,13 +8,7 @@ import {getFirestore} from "firebase/firestore" //! for storin posts
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAJ7rHkEz78cTjnQ3Vt-9uqCCY31ZmsAE0",
-  authDomain: "chaty-40042.firebaseapp.com",
-  projectId: "chaty-40042",
-  storageBucket: "chaty-40042.appspot.com",
-  messagingSenderId: "602322352902",
-  appId: "1:602322352902:web:f99670eccb34d13e76fc87",
-  measurementId: "G-X70FG9VGHQ"
+  Secret
 };
 
 // Initialize Firebase
